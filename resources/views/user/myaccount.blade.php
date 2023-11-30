2ef8e001c1566ba8f479e081da7b0278333911da
Shayan <shayanhpn@gmail.com>
2023-11-30 16:23:40 +0330

    fix my account viwe

--- resources/views/user/myaccount.blade.php
+++ resources/views/user/myaccount.blade.php
@@ -5,14 +5,41 @@
     <div class="card p-2 shadow-sm">
         <div class="card-body">
             <h5 class="card-title text-center fw-bold">حساب کاربری</h5>
-            <h6>نام: <span>شایان</span></h6>
-            <h6>نام خانوادگی: <span>حسین پوریان</span></h6>
+            <h6 class="text-secondary">نام: <span>شایان</span></h6>
+            <h6 class="text-secondary">نام خانوادگی: <span>حسین پوریان</span></h6>
+            <h6 class="text-secondary">تاریخ عضویت: <span>1402/08/07</span></h6>
             <hr>
             <a type="button" href="" class="btn btn-primary w-100">ویرایش اطلاعات</a>
         </div>
     </div>
 </div>
     <div class="col-md-9">
+        <div class="d-flex flex-row justify-content-center">
+            <div class="row">
+                <div class="card p-2 text-bg-white mb-3 rounded-3 shadow-sm" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">تعداد نوشته ها</h5>
+                        <p class="card-text text-secondary" style="font-size: 10px">تعدادکل نوشته های منتشر شده توسط شما</p>
+                        <h4>+50</h4>
+                    </div>
+                </div>
+
+                <div class="card p-2 text-bg-white mb-3 rounded-3 shadow-sm mx-2" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">تعداد نوشته ها</h5>
+                        <p class="card-text text-secondary" style="font-size: 10px">تعدادکل نوشته های منتشر شده توسط شما</p>
+                        <h4>+50</h4>
+                    </div>
+                </div>
+                <div class="card p-2 text-bg-white mb-3 rounded-3 shadow-sm" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">تعداد نوشته ها</h5>
+                        <p class="card-text text-secondary" style="font-size: 10px">تعدادکل نوشته های منتشر شده توسط شما</p>
+                        <h4>+50</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card p-2 shadow-sm">
             <div class="card-body">
                 <h5 class="card-title text-center fw-bold">تنظیمات حساب کاربری</h5>
